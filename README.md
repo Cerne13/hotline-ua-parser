c68a2b87cc2ff3cc2ce51066336ac73499e6abea
Roman Kolmakov <cerne313@gmail.com>
2022-12-21 00:17:17 +0200

    Create readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# Hotline.ua parser
+Simple parser for hotline.ua that gets needed items from certain category 
+and stores data in the specified .csv file.
+
+***
+
+## Installation and usage
+
+- clone the repo:
+```
+git clone https://github.com/Cerne13/hotline-ua-parser.git
+```
+
+- create venv if necessary
+<br></br>
+
+- install dependencies
+```
+pip install -r requirements.txt
+```
+- specify constants (see below)
+<br></br>
+
+- run the parse.py file
+
+***
+
+### Constants
+**BASE_URL**: The first page of the category to be parsed.
+
+**ITEMS_LIST**: Data file used to filter out needed items. Should contain
+all the items you want to get data for (case sensitive) each on a new line.
+Must be in the same directory with parse.py
+
+**OUTPUT_FILE**: Filename of csv file where gathered data will be stored.
+Will appear in the same directory with parse.py.
+If you run the script again, the gathered data will be replaced with newly
+scraped one.
+
+***
+
--- parse.py
+++ parse.py
@@ -6,8 +6,10 @@ import requests
 from bs4 import BeautifulSoup, Tag
 
 BASE_URL = "https://hotline.ua/ua/computer/noutbuki-netbuki/33373/"
+ITEMS_LIST = "items.txt"
+OUTPUT_FILE = "laptops.csv"
+
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
-NOTEBOOKS_LIST = 'items.txt'
 
 
 @dataclass
@@ -93,39 +95,39 @@ def get_laptops_info() -> [Laptop]:
     ]
 
     # pagination
-    # next_page_disabled = soup.select_one("a.page--next.page--disabled")
-    # page = 2
-    #
-    # while next_page_disabled is None:
-    #     print(f"Parsing: {BASE_URL}?p={page}")
-    #
-    #     next_page = requests.get(
-    #         urljoin(BASE_URL, f"?p={page}"),
-    #         headers=HEADERS
-    #     ).content
-    #
-    #     soup = BeautifulSoup(next_page, "html.parser")
-    #     next_page_disabled = soup.select_one("a.page--next.page--disabled")
-    #
-    #     # Just a test item to see if contents is got successfully
-    #     list_item_test = soup.select_one(".list-item__info").name
-    #     print(
-    #         "Contents got successfully"
-    #         if list_item_test is not None
-    #         else "Request blocked. You should try different VPN."
-    #     )
-    #
-    #     page += 1
-    #
-    #     all_laptop_divs = soup.select(".list-item")
-    #     parsed_laptops.extend([
-    #         parse_single_laptop(laptop)
-    #         for laptop in all_laptop_divs
-    #     ])
+    next_page_disabled = soup.select_one("a.page--next.page--disabled")
+    page = 2
 
-    filtered_list = filter_laptops(parsed_laptops, NOTEBOOKS_LIST)
+    while next_page_disabled is None:
+        print(f"Parsing: {BASE_URL}?p={page}")
 
-    return parsed_laptops
+        next_page = requests.get(
+            urljoin(BASE_URL, f"?p={page}"),
+            headers=HEADERS
+        ).content
+
+        soup = BeautifulSoup(next_page, "html.parser")
+        next_page_disabled = soup.select_one("a.page--next.page--disabled")
+
+        # Just a test item to see if contents is got successfully
+        list_item_test = soup.select_one(".list-item__info").name
+        print(
+            "Contents got successfully"
+            if list_item_test is not None
+            else "Request blocked. You should try different VPN."
+        )
+
+        page += 1
+
+        all_laptop_divs = soup.select(".list-item")
+        parsed_laptops.extend([
+            parse_single_laptop(laptop)
+            for laptop in all_laptop_divs
+        ])
+
+    filtered_list = filter_laptops(parsed_laptops, ITEMS_LIST)
+
+    return filtered_list
 
 
 def write_to_file(output_csv_path: str) -> None:
@@ -141,4 +143,4 @@ def write_to_file(output_csv_path: str) -> None:
 
 
 if __name__ == "__main__":
-    write_to_file("laptops.csv")
+    write_to_file(OUTPUT_FILE)
